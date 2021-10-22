53e4dfbcc4f95d802513e82d58b2d13cc3f89a23
Abdelali12-codes <jadelmoulaa3@gmail.com>
2021-10-22 22:20:21 +0100

    password access

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Abdelali
         </p>
         <a
           className="App-link"

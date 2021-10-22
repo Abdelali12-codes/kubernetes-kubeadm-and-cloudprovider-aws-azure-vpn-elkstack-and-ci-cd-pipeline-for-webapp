1f62bb79be1fe1719c0105864143c085d7d35046
Abdelali12-codes <jadelmoulaa3@gmail.com>
2021-10-22 21:41:07 +0100

    password access

--- scripts/copy-files.sh
+++ scripts/copy-files.sh
@@ -2,4 +2,4 @@ cd /home/azureuser/devops
 sudo cp -r build/* nginx
 
 # sudo cp ansible/deployments-services.yml /home/abdelali/
-# sudo scp ansible/deployments-services.yml ubuntu@10.10.3.212:/home/ubuntu/cluster
+scp workloads/dep-service-nginx.yml ubuntu@10.10.1.166:/home/ubuntu/cluster
--- scripts/run-ansible-playbook.sh
+++ scripts/run-ansible-playbook.sh
@@ -1,4 +1,4 @@
 cd /home/azureuser/devops/ansible
 ansible-playbook ansible-build-reactjs-image.yml 
 echo "the ansible build reactjs image finished"
-# ansible-playbook ansible-run-cluster.yml
+ansible-playbook ansible-run-cluster.yml

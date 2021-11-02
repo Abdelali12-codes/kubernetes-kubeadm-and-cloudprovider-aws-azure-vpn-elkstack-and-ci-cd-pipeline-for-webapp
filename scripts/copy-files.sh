6d52717bebe9be27613d102938b73b33cf29797a
Abdelali12-codes <jadelmoulaa3@gmail.com>
2021-11-02 11:59:28 +0100

    new changes

--- scripts/copy-files.sh
+++ scripts/copy-files.sh
@@ -2,4 +2,4 @@ cd /home/azureuser/devops
 sudo cp -r build/* nginx
 
 # sudo cp ansible/deployments-services.yml /home/abdelali/
-scp workloads/dep-service-nginx.yml ubuntu@10.10.1.166:/home/ubuntu/cluster
+scp workloads/dep-service-nginx.yml ubuntu@10.10.1.53:/home/ubuntu/cluster

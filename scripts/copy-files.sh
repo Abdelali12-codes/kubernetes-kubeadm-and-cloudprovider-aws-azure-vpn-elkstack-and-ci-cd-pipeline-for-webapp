28d4843426883cd6abf7b3cbde535d849a8a4ce7
Abdelali12-codes <jadelmoulaa3@gmail.com>
2021-11-02 16:35:49 +0100

    new files

--- scripts/copy-files.sh
+++ scripts/copy-files.sh
@@ -4,4 +4,4 @@ sudo cp -r build/* nginx
 # sudo cp ansible/deployments-services.yml /home/abdelali/
 scp workloads/dep-service-nginx.yml ubuntu@10.10.1.53:/home/ubuntu/cluster
 scp workloads/dep-service-web.yml ubuntu@10.10.1.53:/home/ubuntu/cluster
-scp workloads/ingress-k8s.yml buntu@10.10.1.53:/home/ubuntu/cluster
+scp workloads/ingress-k8s.yml ubuntu@10.10.1.53:/home/ubuntu/cluster
